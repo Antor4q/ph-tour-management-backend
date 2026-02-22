8214b8498a1e0bb95e41e001f49d2d8484bf8285
Antor4q <tariquelislam2015@gmail.com>
2026-02-22 10:48:40 +0600

    refresh token added in cookie

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -4,5 +4,6 @@ import { AuthControllers } from "./auth.controler";
 const router = Router()
 
 router.post("/login", AuthControllers.credentialsLogin)
+router.post("/refresh-token", AuthControllers.getNewAccessToken)
 
 export const AuthRoutes = router;
--- src/app/utils/seedSuperAdmin.ts
+++ src/app/utils/seedSuperAdmin.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { enVars } from "../config/env"
 import { IAuthProvider, IUser, Role } from "../modules/user/user.interface"
 import { User } from "../modules/user/user.model"
